4753579d8e17de47cf7c7f31944b532efd0769c7
rpinto <rachel.lea.pinto@gmail.com>
2018-08-27 08:44:27 -0700

    counting sheep drill

--- recursiondrills.js
+++ recursiondrills.js
@@ -1,1 +1,16 @@
 'use strict'
+
+// //Counting Sheep
+// function countingSheep(totalSheep) {
+
+//   if (totalSheep === 0) {
+//     return '';
+//   } else {
+//       console.log(`${totalSheep} Another sheep jump over the fence`)
+//     countingSheep(totalSheep - 1)
+//     }
+
+//   }
+
+// let totalSheep = 7;
+// countingSheep(7);
